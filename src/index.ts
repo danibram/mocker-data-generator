41a87dbb72c1d18743d43a96a499a34088335b12
Daniel Biedma Ramos <info@dbr.io>
2017-02-26 20:05:44 +0100

    refactor(package): turn to use typescript and typescript starter boiler

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+if (!global._babelPolyfill) {
+   require('babel-polyfill');
+}
+
+import Mocker from './Mocker'
+
+const initMocker = function(opts) {
+    return new Mocker(opts)
+}
+
+module.exports = initMocker
