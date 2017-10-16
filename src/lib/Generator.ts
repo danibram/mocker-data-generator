0b944711f6543394974eb9c6a240c18162fab1a8
Daniel Biedma Ramos <info@dbr.io>
2017-10-16 10:50:59 +0800

    feat(fakerjs): Added multilanguaje support

--- src/lib/Generator.ts
+++ src/lib/Generator.ts
@@ -23,13 +23,17 @@ export class Generator {
         let faker = f
         let db = this.DB
         let object = this.object
-
         let re = /(^[a-z_A-Z]*)/   // en_US
         let matches = re.exec(cfg.locale)
         let strFn
-        if (matches && matches.length === 2) {
-            strFn = 'faker.' + cfg.locale
-            eval(strFn)
+
+        if (cfg.locale) {
+            re = /(^[a-z_A-Z]*)/   // en_US
+            matches = re.exec(cfg.locale)
+
+            if (matches && matches.length === 2) {
+                faker = require('faker/locale/' + cfg.locale)
+            }
         }
 
         re = /(^[a-zA-Z.]*)/   // aZ.aZ
