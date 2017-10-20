3536bdfc6c351b5445c1cfccfc42bb565103e291
Daniel Biedma Ramos <info@dbr.io>
2017-10-20 12:35:04 +0800

    chore(gh-pages): new build

--- _docs_website/next.config.js
+++ _docs_website/next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+    assetPrefix: isProd ? '/mocker-data-generator' : '',
     exportPathMap: function() {
         return {
             '/': { page: '/' }
