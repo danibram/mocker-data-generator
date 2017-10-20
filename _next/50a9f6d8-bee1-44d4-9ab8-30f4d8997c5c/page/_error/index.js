5a7752bd44452f906571f5c6ce0c30a0b6ccb166
Daniel Biedma Ramos <info@dbr.io>
2017-10-20 12:21:50 +0800

    chore(gh-pages): add build

---
+++ _next/50a9f6d8-bee1-44d4-9ab8-30f4d8997c5c/page/_error/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_error', function() {
+            var comp = module.exports=webpackJsonp([5],[],[207]);
+            return { page: comp.default }
+          })
+        
