6b758d31fcddbe76b78d23bd631ef5d38cf4595b
Daniel Biedma Ramos <info@dbr.io>
2017-10-23 11:52:42 +0800

    Updates

--- _next/a35d69a2-0ae6-4ea0-9ed1-fe661d863046/page/index.js
+++ _next/a35d69a2-0ae6-4ea0-9ed1-fe661d863046/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([5],[],[218]);
+            var comp = module.exports=webpackJsonp([5],[],[220]);
             return { page: comp.default }
           })
         
