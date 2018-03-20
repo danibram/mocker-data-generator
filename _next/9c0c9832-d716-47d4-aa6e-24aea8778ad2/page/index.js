3a9ee58de4ef720b12b2250e3fd08123db01dfd3
Daniel Biedma <info@dbr.io>
2018-03-20 14:48:08 +0800

    Updates

--- _next/9c0c9832-d716-47d4-aa6e-24aea8778ad2/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([5],[],[220]);
-            return { page: comp.default }
-          })
-        
